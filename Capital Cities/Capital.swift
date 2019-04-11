e391d34e4d79c08bfc0d926af6fa498630293c81
Michele Galvagno <michicello@gmail.com>
2019-04-11 16:15:49 +0200

    MapKit first steps

---
+++ Capital Cities/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Michele Galvagno on 11/04/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
